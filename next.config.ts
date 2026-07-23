9123a35f486833b939facfe123d70be1c9fa03e0
dzgoodme <659399268@qq.com>
2026-07-23 15:45:28 +0800

    Update Pages path for renamed repository

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   output: "export",
-  basePath: process.env.GITHUB_ACTIONS ? "/GrooveParty-Official-Web-Final" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/GrooveParty-Web" : "",
 };
 
 export default nextConfig;
